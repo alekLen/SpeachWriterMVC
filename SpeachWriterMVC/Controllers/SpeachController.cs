ae4b3d177d67b8413d41a71baf8c7e4f88b7c765
alekLen <aleklenoch@gmail.com>
2024-05-27 14:51:51 +0300

    #запись аудиофайла

--- SpeachWriterMVC/Controllers/SpeachController.cs
+++ SpeachWriterMVC/Controllers/SpeachController.cs
@@ -5,25 +5,48 @@ using SpeachWriterMVC.Models;
 
 namespace SpeachWriterMVC.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class SpeachController : Controller
     {
         async public Task<IActionResult> ToWrite()
         {
             MyText model = new MyText();
-            try
+
+            //try
+            //{
+            //    var speechConfig = SpeechConfig.FromSubscription("ac8923a842584dbfa1ff8613106ee5dd", "eastus");
+            //    speechConfig.SpeechRecognitionLanguage = "ru-RUS";
+
+            //    using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            //    using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            //    var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+            //    model.text = OutputSpeechRecognitionResult(speechRecognitionResult);
+            //}
+            //catch(Exception ex) { model.text = ex.Message; }
+
+            var speechConfig = SpeechConfig.FromSubscription("ac8923a842584dbfa1ff8613106ee5dd", "eastus");
+             speechConfig.SpeechRecognitionLanguage = "ru-RUS";
+            try {  
+            var audio = Request.Form.Files["audio"];
+            if (audio != null)
             {
-                var speechConfig = SpeechConfig.FromSubscription("ac8923a842584dbfa1ff8613106ee5dd", "eastus");
-                speechConfig.SpeechRecognitionLanguage = "ru-RUS";
+                var filePath = Path.Combine("Uploads", "recording.wav");
 
-                using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
-                using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await audio.CopyToAsync(stream);
+                }
 
-                var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
-                model.text = OutputSpeechRecognitionResult(speechRecognitionResult);
             }
-            catch(Exception ex) { model.text = ex.Message; }
-         
-            return View(model);
+            using var audioConfig = AudioConfig.FromWavFileInput("recording.wav");
+            using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+            var result = await speechRecognizer.RecognizeOnceAsync();
+            model.text=result.Text;
+             }
+             catch(Exception ex) { model.text = ex.Message; }
+                return View(model);
         }
 
         static string OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
